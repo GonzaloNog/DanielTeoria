376e1cb0340276335254aebe14e982e72f7ba30b
Rayphox <danirper05@gmail.com>
2025-02-04 17:32:16 +0000

    Ejercicio

---
+++ Assets/Ejercicio1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+
+    public bool encendido = true;
+    public float numero1 = 8.45f;
+    public float numero2 = 6.09f;
+    public string operaciones = "null";
+    void Start()
+    {
+        if (encendido == true)
+        {
+            
+        }
+        else
+        {
+            Debug.Log("Apagada");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
