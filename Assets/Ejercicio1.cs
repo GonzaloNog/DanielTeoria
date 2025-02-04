1bcc5c072cdede33dcb5c0e41f02c2e8c63ae182
Rayphox <danirper05@gmail.com>
2025-02-04 18:02:04 +0000

    Ejercicio1Final

--- Assets/Ejercicio1.cs
+++ Assets/Ejercicio1.cs
@@ -9,10 +9,44 @@ public class Ejercicio1 : MonoBehaviour
     public float numero1 = 8.45f;
     public float numero2 = 6.09f;
     public string operaciones = "null";
+    private float resultado;
+    private bool resultadoOperacion = true;
     void Start()
     {
         if (encendido == true)
         {
+            switch (operaciones)
+            {
+                case "suma":
+                    //Debug.Log(numero1 + numero2);
+                    //numero1 += numero2;
+                    //Debug.Log(numero1);
+                    resultado = numero1 + numero2;
+                    
+
+                    break;
+                case "resta":
+                    resultado = numero1 - numero2;
+
+                    break;
+                case "multiplicar":
+                    resultado = numero1 * numero2;
+
+                    break;
+                case "dividir":
+                    resultado = numero1 / numero2;
+
+                    break;
+                default:
+                    Debug.Log("Operación no disponible");
+                    resultadoOperacion =false;
+
+                    break;
+            }
+            if (resultadoOperacion ==true)
+            {
+                Debug.Log(resultado);
+            }
             
         }
         else
